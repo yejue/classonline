ccd24faa74e57936fa09818be29626ed4cf6aae6
yejue <yejue@yjstudy1.com>
2020-05-07 00:30:17 +0800

    增加两个 ajax用的url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from .views import login, Register
+from django.urls import path, re_path
+from .views import login, Register, check_username_view, check_mobile_view
 
 
 app_name = 'user'
@@ -7,5 +7,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('register/', Register.as_view(), name='register')
+    path('register/', Register.as_view(), name='register'),
+    re_path('check=(?P<username>\w{5,20})/', check_username_view, name='check_username'),
+    re_path('mobile=(?P<mobile>1[3456789]\d{9})/', check_mobile_view, name='check_mobile'),
 ]
