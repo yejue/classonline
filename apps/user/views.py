f5e6d3556d6b8bf0e64f16be3d0bf8c028a5d7d8
yejue <yejue@yjstudy1.com>
2020-05-18 20:32:01 +0800

    完善了注册功能视图

--- apps/user/views.py
+++ apps/user/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 
+
+from .forms import RegisterForm
 from .models import User
+from utils.res_code import Code, error_map
 from utils.genCheckJson import genCheckJson
-
-
 # Create your views here.
 
 
@@ -23,36 +24,20 @@ class Register(View):
         return render(request, 'user/register.html')
 
     def post(self, request):
-        pass
+        # 1.校验数据
+        form = RegisterForm(request.POST, request=request)
+        if form.is_valid():
+            # 新建数据
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            mobile = form.cleaned_data.get('mobile')
+            # 新建用户
+            User.objects.create_user(username=username, password=password, mobile=mobile)
+            return JsonResponse(genCheckJson(errmsg='注册成功'))
+        else:
+            # 将表单错误信息进行拼接
+            err_msg_str = '/'.join([item[0] for item in form.errors.values()])
+
+            return JsonResponse(genCheckJson(errorno=Code.PARAMERR, errmsg=err_msg_str), charset='utf8')
 
 
-def check_username_view(request, username):
-    """
-    查询用户名是否存在
-    url: /user/check=(?P<username>\w{5,20})
-    :param request:
-    :param username:
-    :return:
-    """
-    data = {
-            "username": username,   # 查询用户名
-            "count": User.objects.filter(username=username).count()     # 查询用户数量
-        }
-
-    return JsonResponse(genCheckJson(data=data), charset='utf8')
-
-
-def check_mobile_view(request, mobile):
-    """
-    查询手机号是否存在
-    url:  /user/mobile=(?P\<mobile>1[3456789]\d{9}/
-    :param request:
-    :param mobile:
-    :return:
-    """
-
-    data = {
-        'mobile': '18666996101',
-        'count': User.objects.filter(mobile=mobile).count()
-    }
-    return JsonResponse(genCheckJson(data=data), charset='utf8')
