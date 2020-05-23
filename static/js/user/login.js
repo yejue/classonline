4f3893b18b639a374ef6fae1dcf8070d40be82d5
yejue <yejue@yjstudy1.com>
2020-05-23 15:43:22 +0800

    修改了一些bug

--- static/js/user/login.js
+++ static/js/user/login.js
@@ -5,29 +5,28 @@ let isAccountReady = false,    // 用户名状态
 
 // 用户名校验 onblur
 let $account = $('input[name="account"]');
+$account.blur(checkAccount);
 
-function checkAccount(msg='') {
+function checkAccount() {
     isAccountReady = false;
     // 取得用户名
     $sAccount = $account.val();
 
     if (!$sAccount) {
-        if(msg){
-            message.showError(msg)
-        }
+        // message.showError()
     } else if (!(/^\w{5,20}$/).test($sAccount)) {
         message.showError('请输入5~20位的字母或数字用户名');
+    }else{
+        isAccountReady = true
     }}
 // 密码
 let $password = $('input[name="password"]');
-
+$password.blur(checkLoginPassword);
 // 密码校验
-function checkLoginPassword(msg=''){
+function checkLoginPassword(){
     isPasswordReady = false;
     if (!$password.val()){
-        if(msg){
-            message.showError(msg)
-        }
+        // message.showError(msg)
     }else{
         isPasswordReady = true
     }
@@ -38,16 +37,18 @@ function checkLoginPassword(msg=''){
 let $loginBtn = $('.login-btn');
 $loginBtn.click(loginFn);
 
+
 function loginFn(e) {
     e.preventDefault();
     if (!isAccountReady) {
-        checkAccount('用户名或密码不能为空');
+        checkAccount();
+        message.showError('用户名或密码不能为空');
         return
     } else if (!isPasswordReady) {
-        checkLoginPassword('用户名或密码不能为空');
+        checkLoginPassword();
+        message.showError('用户名或密码不能为空');
         return
     }
-
     $.ajax({
         url: '/user/login/',
         type: 'POST',
@@ -58,7 +59,7 @@ function loginFn(e) {
         }
     })
         .done(function (res) {
-            if (res['error'] !== 0) {
+            if (res['error'] != 0) {
                 message.showError(res['errmsg'])
             } else {
                 message.showSuccess('登录成功，正在跳转')
