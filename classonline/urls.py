83829e8c393736c48c8a03442fafbd45825a50d1
yejue <yejue@yjstudy1.com>
2020-05-23 21:50:19 +0800

    media路径增加

--- classonline/urls.py
+++ classonline/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import settings, static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('veri/', include('verification.urls')),
     # path('', include('user.urls')),
     path('', include('news.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
