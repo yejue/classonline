b164954da7a8c84db914d3068aeef117fca99175
yejue <yejue@yjstudy1.com>
2020-05-23 21:48:59 +0800

    增加了新闻列表url

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, search
+from .views import index, search, NewsListView
 
 app_name = 'news'
 
 urlpatterns = [
     path('', index, name='index'),
     path('search/', search, name='search'),
+    path('news/', NewsListView.as_view(), name='news_list'),
 ]
